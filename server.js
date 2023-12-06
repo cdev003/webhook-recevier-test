0d7b45a4c87ab172aa1ce536faa4efa7ba43db17
cdev003 <dmytro@reseed.farm>
2023-12-05 16:16:09 -0800

    Set railyway setting

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 const PORT = 5000;
 
 app.use(cors());
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
