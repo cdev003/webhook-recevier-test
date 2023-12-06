3162383051c993c10d5ab335159234c03c7d985a
cdev003 <dmytro@reseed.farm>
2023-12-05 16:03:41 -0800

    Add cors filter

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const http = require('http');
 const app = express();
 const PORT = 5000;
+
+app.use(cors());
 
 const server = http.createServer(app);
 
